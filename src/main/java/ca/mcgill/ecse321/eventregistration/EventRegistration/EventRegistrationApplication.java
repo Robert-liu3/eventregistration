bc74ba592d9f1d35839e764519ed2cb50b1bf0c3
Robert-liu3 <robertliu80@gmail.com>
2021-09-17 18:24:32 -0400

    Initial commit of the backend application

---
+++ src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistration/EventRegistrationApplication.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse321.eventregistration.EventRegistration;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@RestController
+@SpringBootApplication
+public class EventRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world!";
+	}
+}
