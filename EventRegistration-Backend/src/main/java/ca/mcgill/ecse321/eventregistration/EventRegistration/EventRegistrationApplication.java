625cfd90a00547acaf42fbee91f021b357322ab3
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2021-10-08 13:33:28 -0400

    Update EventRegistrationApplication.java

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistration/EventRegistrationApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistration/EventRegistrationApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 public class EventRegistrationApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EventRegistrationApplication.class, args);
 	}
 
